78c7d5be7ffe9af609bd31a7d949a59505a0a155
Manuel Méndez <troyanlp@gmail.com>
2022-03-20 23:35:12 +0100

    Changed sprites and set up tetromino data

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+
+    public TetrominoData[] tetrominoData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
